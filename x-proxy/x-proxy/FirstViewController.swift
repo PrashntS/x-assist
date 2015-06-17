1bf844b29c9f2ce7796a8a58c5904da2b25018ec
Prashant Sinha <prashnts@live.com>
2015-06-17 09:03:18 +0530

    Created new xcode project

---
+++ x-proxy/x-proxy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  x-proxy
+//
+//  Created by Prashant Sinha on 17/06/15.
+//  Copyright (c) 2015 Prashant Sinha. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ x-proxy/x-proxy/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  x-proxy
+//
+//  Created by Prashant Sinha on 17/06/15.
+//  Copyright (c) 2015 Prashant Sinha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
